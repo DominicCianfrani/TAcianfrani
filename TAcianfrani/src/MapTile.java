1280ba857bdf5459c5728cce3ff4318aeed0db9e
DominicCianfrani <drc5685@psu.edu>
2023-03-12 23:05:01 -0400

    Actions

--- TAcianfrani/src/MapTile.java
+++ TAcianfrani/src/MapTile.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class MapTile {
     int x;
     int y;
@@ -7,29 +9,17 @@ public class MapTile {
         this.y = y;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + x;
-        result = prime * result + y;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MapTile other = (MapTile) obj;
-        if (x != other.x)
-            return false;
-        if (y != other.y)
-            return false;
-        return true;
-    }
+        public ArrayList<Action> adjacent_moves(){
+        ArrayList<Action> moves = new ArrayList<Action>();
+        if (World.tile_exists(x , y +1) != null)
+            moves.add(new MoveEast());
+        if (World.tile_exists(x , y -1) != null)
+            moves.add(new MoveWest());
+        if (World.tile_exists(x -1, y) != null)
+            moves.add(new MoveNorth());
+        if (World.tile_exists(x +1, y) != null)
+            moves.add(new MoveSouth());
+        return moves;
+        }
 
 }
