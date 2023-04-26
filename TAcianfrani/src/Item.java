0d97f593e6ae43fbc9960f0dc906e44ed6f6a960
dominiccianfrani <drc5685@psu.edu>
2023-04-26 13:16:00 -0400

    Inventory can be accessed now

--- TAcianfrani/src/Item.java
+++ TAcianfrani/src/Item.java
@@ -4,35 +4,39 @@ public class Item {
     protected String desc;
     protected int value;
 
-    public Item(){
+    public Item() {
     }
-    public Item(String name, String desc, int value){
+
+    public Item(String name, String desc, int value) {
         this.name = name;
         this.desc = desc;
         this.value = value;
     }
-    public Item(String name){
+
+    public Item(String name) {
         this.name = name;
         this.desc = "Dominic Cianfrani";
         this.value = 15;
     }
-    public Item(String desc, int value){
+
+    public Item(String desc, int value) {
         this.desc = desc;
         this.value = value;
     }
-    public Item(int value){
+
+    public Item(int value) {
         this.value = value;
     }
 
-    public void setName(String nameStr){
+    public void setName(String nameStr) {
         this.name = nameStr;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public String toString(){
-        return String.format("%s \n=====\nValue: %d \n", this.name, this.desc, this.value);
+    public String toString() {
+        return String.format("%s \n=====\nDescription: %s \nValue: %d \n", this.name, this.desc, this.value);
     }
 }
